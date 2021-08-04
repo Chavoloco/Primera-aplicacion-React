db43910172693e83f644a86a471d37d952211a64
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-08-04 15:07:39 -0300

    Creación del proyecto

---
+++ books-store/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Menu from './Menu';
+import List from './List';
+
+function App() {
+  return (
+   <div className="app">
+     <Menu />
+     <List />
+   </div>
+  );
+}
+
+export default App;
